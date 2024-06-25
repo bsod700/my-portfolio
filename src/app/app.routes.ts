9ece7136ead01d8038747b41ac1d67bf02eaaf87
BSoD700 <guytagger@gmail.com>
2024-06-25 15:16:56 +0300

    add icon + styles

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { 
+        path: '', 
+        loadComponent:() => import('./pages/main/main.component').then(mod  => mod.MainComponent),
+        data: { name: 'Guy Tagger Portfolio' }
+    },
+    { path: '**', redirectTo: '/' },
+];
